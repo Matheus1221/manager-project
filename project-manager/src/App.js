59d664c6e4885f8a514bab400d66f3879ade9eb0
Matheus1221 <58350861+Matheus1221@users.noreply.github.com>
2022-11-25 13:58:23 -0300

    Adicionado estrutura básica no projeto

--- project-manager/src/App.js
+++ project-manager/src/App.js
@@ -1,24 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
 
+import Container from "./components/layout/Container";
+
 function App() {
   return (
     <div className="App">
       <Router>
-        <ul>
-          <li>Home</li>
-          <li>Contato</li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contato" element={<Contact />} />
-          <Route path="/company" element={<Company />} />
-          <Route path="/newproject" element={<NewProject />} />
-        </Routes>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/company">Company</Link>
+          <Link to="/newproject">NewProject</Link>
+        </div>
+        <Container customClass="minHeight">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/newproject" element={<NewProject />} />
+          </Routes>
+        </Container>
         <p>Footer</p>
       </Router>
     </div>
