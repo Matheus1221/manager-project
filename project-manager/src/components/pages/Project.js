6c26bda468db6a79fa18e9a56186967faac3b0eb
Matheus1221 <58350861+Matheus1221@users.noreply.github.com>
2022-12-13 15:30:37 -0300

    Pagina de edit visualização de detalhes do projeto finalizada

--- project-manager/src/components/pages/Project.js
+++ project-manager/src/components/pages/Project.js
@@ -5,26 +5,73 @@ import { useState, useEffect } from "react";
 
 import styles from "./Project.module.css";
 
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
+
 function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState([]);
+  const [showProjectFrom, setShowProjectFrom] = useState(false);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setProject(data);
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+        },
       })
-      .catch((err) => console.log(err));
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data);
+        })
+        .catch((err) => console.log(err));
+    }, 2000);
   }, [id]);
 
-  return <p>Project Edit</p>;
+  function toggleProjectForm() {
+    setShowProjectFrom(!showProjectFrom);
+  }
+
+  return (
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            <div className={styles.details_container}>
+              <h1>Project: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectFrom ? "Edit Project" : "Fechar"}
+              </button>
+              {!showProjectFrom ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Category:</span>
+                    {project.category
+                      ? project.category.name
+                      : project.category}
+                  </p>
+                  <p>
+                    <span>Total Budget:</span>R${project.budget}
+                  </p>
+                  <p>
+                    <span>Total Budget Used:</span>R${project.projectmanager}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <p>form</p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
 
 export default Project;
